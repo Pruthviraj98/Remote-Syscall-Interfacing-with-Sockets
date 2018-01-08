95acb4a5b9cd960e56eabdbe728f35eff93e9278
Pruthviraj Patil <pruthvirajr.patil2016@vitstudent.ac.in>
2018-01-08 16:27:51 +0530

    edit 8/1/18

--- sys.c
+++ sys.c
@@ -4,11 +4,12 @@
 
 char* cmdprint=">>>";
 char folder[256]="";
-
-char* findloc(char* command[]) // function to seperate the linux command and fetch back only the directory part.
+/*
+int findloc(char* command[]) // function to seperate the linux command and fetch back only the directory part.
 {
+int i;
 int flag=0;
-for(int i=0;i<strlen(command);i++)
+for( i=0;i<strlen(command);i++)
 {
 flag++;
 if(command[i]==' ')
@@ -31,11 +32,12 @@ folder[i]=command[count++];
 folder=strcat(folder, command);
 return "y";
 }
-
+*/
 int main(int argc, char *argv[])
 {
-while(0)
+for(;;)
 {
+
     printf("%s", cmdprint);
     char inputcommand[256];
     scanf("%s", inputcommand);
@@ -43,23 +45,49 @@ while(0)
 	if(strstr("end", inputcommand))//we assume 'end'  is the keyword for stopping the file navigation. 
 	exit(0);
     
-	char* ctr="";
+	
     if(strstr("cd", inputcommand))
 	{
 	system(inputcommand);
-//	folder=strcat(folder, findloc(inputcommand));
-        ctr=findloc(inputcommand);
-	folder=strcat(folder, "cd");//tobe changed here
-	cmdprint=strcat(cmdprint, folder); //to make the client feel apparently that we are into that folder or directory
-	cmdprint=strcat(cmdprint, "/");
+	
+	//edit 8/1/18
+	
+	int i;
+	int flag=0;
+	for( i=0;i<strlen(inputcommand);i++)
+	{
+	flag++;
+	if(inputcommand[i]==' ')
+	{
+	break;
+	}
+	}
+	
+	
+	if(flag==0)
+	{
+	strcpy(folder, "");	
+	}
+	else
+	{
+	int count=0;
+	for(i=flag;i<strlen(inputcommand);i++)
+	{
+	folder[i]=inputcommand[count++];
+	}
+	
+//	strcat(folder, inputcommand);
+	}
+	//end edit 8/1/18
+
+	strcat(folder, "cd");
+	strcat(cmdprint, folder); //to make the client feel apparently that we are into that folder or directory
+	strcat(cmdprint, "//");
 	continue;
 	}
-    
-    if(ctr=="n")
-	{
-	cmdprint=">>>";
-	} 
-    if(strcmp(">>>", cmdprint)==0)//to go to the perticular directory and execute the commands
+    else
+    	{  
+        if(strcmp(">>>", cmdprint)==0)//to go to the perticular directory and execute the commands
 	{
 	system(inputcommand);//if no change in directory has occured, we directly execte the input commands;
 	printf("\n");
@@ -67,10 +95,12 @@ while(0)
      else
 	{
 	char* a=strcat("chdir ", folder);//to get according to syntax : 'chdir directoryname && chdir... && inputcommand' is executed 
-	a=strcat(a, "&&");
-	char* b=strcat(a, inputcommand);	
-	system(b);
+	strcat(a, "&&");
+	strcat(a, inputcommand);	
+	system(a);
 	}
-    return 0;
+	
+	
+	}
 }
 }
