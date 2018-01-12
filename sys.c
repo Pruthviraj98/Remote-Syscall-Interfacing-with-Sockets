1d810ca72e0cb71cabd6d6ad2ca1a55c7b7aee1a
Pruthviraj Patil <pruthvirajr.patil2016@vitstudent.ac.in>
2018-01-12 01:26:29 -0800

    Update sys.c

--- sys.c
+++ sys.c
@@ -10,23 +10,18 @@ char folder[256]="";
 for(;;)
 {
 
-    printf("%s", cmdprint);
     size_t buffersize=256;
     char *inputcommand=(char*)malloc(buffersize*(sizeof(char)));
-    
-    getline(&inputcommand, &buffersize, stdin);
 
-	if(strstr(inputcommand, "end"))//we assume 'end'  is the keyword for stopping the file navigation.
+    printf("%s", cmdprint);    
+    getline(&inputcommand, &buffersize, stdin);
+	if(strstr(inputcommand, "end"))
 	{
 	exit(1);
-	}
-
-    if(strstr("cd", inputcommand))
-	{
-	system(inputcommand);
-
-	//edit 8/1/18
-
+	}	
+	
+if(strstr(inputcommand, "cd"))
+{
 	int i;
 	int flag=0;
 	for( i=0;i<strlen(inputcommand);i++)
@@ -36,45 +31,56 @@ for(;;)
 	{
 	break;
 	}
-	}
+	}	
 
+printf("\n%s", folder);
 
-	if(flag==0)
-	{
-	strcpy(folder, "");
-	}
-	else
-	{
-	char folderappend[256];
-	int countappend=0;
+char folderappend[256];
+int countappend=0;
 	for(i=flag;i<strlen(inputcommand);i++)
 	{
 	folderappend[countappend++]=inputcommand[i];
 	}
+strncat(folder, folderappend, countappend);
+strncat(cmdprint, folderappend, countappend);
+}
 
-	strncat(folder, folderappend, countappend);
-	strncat(cmdprint, folderappend, countappend);		
-	strcat(folder, "/");
-	}
 
-	
-	}
-    else
+    else//for commands except navigation
     {
-    
-	if(strcmp(">>>", cmdprint)==0)//to go to the perticular directory and execute the commands
+	if(strlen(cmdprint)==3)//to go to the perticular directory and execute the commands
 	{
-	system(inputcommand);//if no change in directory has occured, we directly execte the input commands;
+	//system(inputcommand);//if no change in directory has occured, we directly execte the input commands;
 	printf("\n");
 	}
-    else
+        else
 	{
-	char* a=strcat("chdir ", folder);//to get according to syntax : 'chdir directoryname && chdir... && inputcommand' is executed
-	strcat(a, "&&");
-	strcat(a, inputcommand);
-	system(a);
-	}
+//to get according to syntax : 'chdir directoryname && chdir... && inputcommand' is executed
+	char cdcommand[256]="cd ";
+	strncat(cdcommand, folder, strlen(folder));
+	strcat(cdcommand, " && ");
+	strncat(cdcommand, inputcommand, strlen(inputcommand));
+	printf("%s", cdcommand);	
+	
+	int x;
 
+	char finalcommand[256];
+
+	for(x=0;x<strlen(cdcommand);x++)
+	{
+	if(cdcommand[x]=='\n')
+	{
+	finalcommand[x]=' ';	
+	cdcommand[x]==' ';
+	continue;
+	}
+	finalcommand[x]=cdcommand[x];
+	}
+printf("\n-------command executed-------\n\n%s\n",finalcommand);
+
+
+//system(cdcommand);
+	}
     }
 }
 }
